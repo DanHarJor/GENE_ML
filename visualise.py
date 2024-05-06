66cf6bd0bae0c969659753dcac79696416fa2f20
Daniel Harley Jordan <daniel.harley.jordan@outlook.com>
2024-05-06 17:39:59 +0300

    I added some code to include GPy errors on predictions

--- visualise.py
+++ visualise.py
@@ -1,14 +1,27 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-def residuals(y_true, y_predicted, var_name, title=None):
+def residuals(y_true, y_predicted, var_name, title=None, y_pred_err=None):
+    mse=np.mean((y_true-y_predicted)**2)
+
+    
+    
+    
     ncol = 2
-    fig, [ax1, ax2] = plt.subplots(1,ncol, figsize=(3*ncol,3))
-    ax1.scatter(y_true, y_predicted)
+    size=6
+    fig, [ax1, ax2] = plt.subplots(1,ncol, figsize=(size*ncol,size))
+    
+    cb = ax1.scatter(y_true, y_predicted, marker='X', c=y_pred_err, cmap='summer')
+    if type(y_pred_err)!=type(None):
+        fig.colorbar(cb, ax=ax1)
     ref = np.linspace(0,np.max((np.max(y_true),np.max(y_predicted))), 100).astype(int)
-    ax1.plot(ref,ref, 'g')
+    ax1.plot(ref,ref, 'r')
     ax1.set_xlabel(f'True {var_name}')
     ax1.set_ylabel(f'Predicted {var_name}')
+    ax1.annotate(f'MSE: {np.round(mse,3)}',
+            xy=(.65, .02), xycoords='axes fraction',fontsize=10)
+            # horizontalalignment='left', verticalalignment='top',
+            # fontsize=5)
     
     ax2.hist(y_true-y_predicted)
     ax2.set_xlabel(f'Residuals, {var_name}')
