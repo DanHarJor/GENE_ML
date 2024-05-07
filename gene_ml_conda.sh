b035ec00c1cc9749d9c59fddf56c296c43f8bea5
Daniel Harley Jordan <daniel.harley.jordan@outlook.com>
2024-05-07 18:09:29 +0300

    deleted, actually moved to docker_conda folder

--- gene_ml_conda.sh
+++
@@ -1,4 +0,0 @@
-conda create -n gene_ml -c conda-forge -c pytorch -c anaconda optuna ipykernel matplotlib pandas numpy scikit-learn tqdm nb_conda_kernels f90nml
-conda activate gene_ml
-pip install GPy==1.13.1
-conda env export --name gene_ml > gene_ml.yml
