903aa1a8142ac1f238481fda973263304c9a2006
danharjor <daniel.harley.jordan@outlook.com>
2024-05-10 18:43:37 +0300

    I moved the parsing to the parser and added the ability to load multiple scan.log files from a scanfiles set of folders

--- dataset/ScanData.py
+++ dataset/ScanData.py
@@ -13,77 +13,119 @@ except:
         raise ImportError 
 
 class ScanData(DataSet):
-    def __init__(self, name, ssh_path=None, test_percentage=50, random_state=47):
+    def __init__(self, name, parser, host=None, remote_path=None, test_percentage=50, random_state=47):
         print('Initialising dataset')
         self.name = name
-        self.scan_log_path = os.path.join(os.getcwd(), 'scanlogs', f'{self.name}_scan.log')
-        self.df = None
-        self.head = None
-        self.x = None
-        self.growthrates = None
-        self.frequencies = None
-        if ssh_path==None:
-            self.load_from_file(self.scan_log_path)
-        else:
-            self.load_from_remote(ssh_path) 
-        print(f'RANDOMLY SPLITTING DATA INTO TEST AND TRAINING SETS: {test_percentage}% test, {100-test_percentage} training.')
-        self.x_train, self.x_test, self.growthrate_train, self.growthrate_test, self.frequencies_train, self.frequencies_test = train_test_split(self.x, self.growthrates, self.frequencies, test_size=test_percentage/100, random_state=random_state)
+        self.parser = parser
+        self.host=host
+        self.remote_path = remote_path
+        ssh_path = f"{host}:{remote_path}"
+        self.scan_log_path = os.path.join(os.getcwd(), 'scanlogs', self.name)
+        if not os.path.exists(self.scan_log_path): os.mkdir(self.scan_log_path)
 
-    def load_from_file(self,data_path):
-        print(f'LOADING SCAN LOG INTO PYTHON {data_path}')
-        self.parse_scan_log(self.scan_log_path)
-        return self.df
+        if remote_path!=None:
+            self.retrieve_remote_logs(ssh_path)
 
-    def load_from_remote(self,ssh_path):
-        print('RETRIEVING SCANLOG VIA scp FROM REMOTE')
-        print(f'scp, scanlog path: {ssh_path}')
-        subprocess.run(['scp',ssh_path, os.path.join(os.getcwd(),'scanlogs')])
-        subprocess.run(['mv','scanlogs/scan.log',self.scan_log_path])
-        print(f'FILE RETRIEVED AND SAVED TO:{self.scan_log_path}')
-        self.load_from_file(self.scan_log_path)
-        return self.df
-    
-    def parse_scan_log(self, scan_log_path=os.path.join('/scratch/project_462000451/daniel/AUGUQ/scanfiles0002/scan.log')):
-        growthrate = []
-        frequency = []
-        head = open(scan_log_path, 'r').readline()
-        # head.replace('\n','')
-        head = head.split('|')
-        last_two = head[-1].split('/')
-        del head[-1]
-        head = head + last_two
-        
-        self.df = pd.read_csv(scan_log_path, sep='|',skiprows=1, names=head)
-        for i in range(len(self.df)):
-            split = self.df[head[-1]][i].lstrip().rstrip().split(' ')      
-            growthrate.append(split[0])
-            frequency.append(split[-1])
-        self.df['growthrate'] = growthrate
-        self.df['frequency'] = frequency
-        self.df = self.df.drop(columns=[head[0],head[-1]])
-        
-        #removing NAN's
-        nan_mask = ~np.isnan(self.df['growthrate'].to_numpy(dtype=float))
-        print('NaN percentage = ',np.sum(nan_mask)*100/len(nan_mask))
-        self.df = self.df.loc[nan_mask]
+        if not os.path.exists(self.scan_log_path): raise FileNotFoundError
 
-        self.df = self.df
+        if os.path.isfile(self.scan_log_path):
+            print('\nLOADING FROM SCANLOG FILE')
+            self.df = self.load_from_file(self.scan_log_path)
+            self.df, n_samp, n_requested, n_samp_nonan = self.remove_nans(self.df)
+            
+        elif os.path.isdir(self.scan_log_path):
+            print('\nLOADING BATCHES FROM SCANLOG DIR')
+            self.df, n_samp, n_requested, n_samp_nonan = self.load_from_dir(self.scan_log_path)
+
+        else: 
+            raise FileNotFoundError        
+
+        print(f'\n{n_samp} SAMPLES RAN OUT OF {n_requested} BEFORE MAX WALLTIME:')
+        print("NUMBER OF SAMPLES AFTER REMOVING NaN's:", n_samp_nonan)
+        nan_percentage = (n_samp-n_samp_nonan)*100/n_samp
+        print('NaN PERCENTAGE = ', nan_percentage)
+
         self.head = list(self.df.columns)
         self.x = self.df[self.head[0:-2]].to_numpy(dtype=float)
         self.growthrates = self.df['growthrate'].to_numpy(dtype=float)
         self.frequencies = self.df['frequency'].to_numpy(dtype=float)
-        return self.df
+        
+        print(f'\nRANDOMLY SPLITTING DATA INTO TEST AND TRAINING SETS: {test_percentage}% test, {100-test_percentage} training.')
+        self.x_train, self.x_test, self.growthrate_train, self.growthrate_test, self.frequencies_train, self.frequencies_test = train_test_split(self.x, self.growthrates, self.frequencies, test_size=test_percentage/100, random_state=random_state)
 
+
+    def load_from_file(self,data_path, verbose=True):
+        print(f'\nLOADING SCANLOG INTO PYTHON {data_path}')
+        df = self.parser.read_output_file(data_path)
+        return df
+
+    def retrieve_remote_logs(self,ssh_path):
+        print('\nRETRIEVING SCANLOG/S VIA scp FROM REMOTE')
+        print(f'SCANLOG PATH: {ssh_path}')
+        # subprocess.run(['scp','-r',ssh_path, self.scan_log_path])
+        if 'scan.log' in self.scan_log_path: #Then we are only taking one file
+            os.system(f'scp -r {ssh_path} {self.scan_log_path}')
+        else: # we should have a directory and need to take all scan logs
+            print('RETRIVING FROM REMOTE DIR')
+            result = 0
+            i = 0
+            while result==0:
+                result = os.system(f"scp '{ssh_path}/scanfiles*{i}/scan.log' {os.path.join(self.scan_log_path,f"scan___{i}.log")}")
+                # result = os.system(f"scp 'lumi:/scratch/project_462000451/gene_out/gene_auto/testing_batchscans/scanfiles*{i}/scan.log' $PWD/scanlogs/testing_batchscans/scan{i}.log")
+    
+                i+=1
+            print('THE ABOVE ERROR IS EXPECTED')
+        
+        print(f'SCANLOG/S RETRIEVED AND SAVED TO:{self.scan_log_path}')
+        
+
+    def load_from_dir(self, data_path):
+        '''
+        This function assumes the directory only contains log files
+        '''
+        if not os.path.isdir(data_path): raise NotADirectoryError
+        
+        dfs, n_samp_all, n_requested_all, n_samp_nonan_all = [], [], [], []
+        for scanlog in os.listdir(data_path):
+            df = self.load_from_file(os.path.join(data_path,scanlog))
+            df, n_samp, n_requested, n_samp_nonan = self.remove_nans(df)
+            dfs.append(df); n_samp_all.append(n_samp); n_requested_all.append(n_requested); n_samp_nonan_all.append(n_samp_nonan)
+        return pd.concat(dfs), np.sum(n_samp_all), np.sum(n_requested_all), np.sum(n_samp_nonan_all)
+        
+    def remove_nans(self, df):
+        ## caution, can only work for df created from single file.
+        #removing NAN's
+        nan_mask = ~np.isnan(df['growthrate'].to_numpy(dtype=float))
+        print('NAN MASK',nan_mask)
+        if len(np.argwhere(nan_mask))>0: n_before_tlimit = int(np.argwhere(nan_mask)[-1])
+        else: n_before_tlimit = 0
+        n_requested = len(df)
+        df = df[0:n_before_tlimit]
+        nan_mask = nan_mask[0:n_before_tlimit]
+        n_samp = len(df)
+        df = df.loc[nan_mask]
+        n_samp_nonan = len(df)
+        return df, n_samp, n_requested, n_samp_nonan
 if __name__ == '__main__':
-    data_set = ScanData(name='100_3p')
-    #df = data_set.load_from_remote('lumi:/scratch/project_462000451/gene_out/ped2_safescan/scanfiles0012/scan.log')
-    df = data_set.load_from_file(os.path.join('scanlogs','100_3p_scan.log'))
+    sys.path.append('/home/djdaniel/DEEPlasma/GENE_ML/gene_ml')
+    from parsers.GENEparser import GENE_scan_parser
+    base_params_path = os.path.join(os.getcwd(),'parameters_base_dp')
+    remote_save_dir='/project/project_462000451/gene_out/gene_auto'
+    parser = GENE_scan_parser(base_params_path, remote_save_dir)
+
+    # ssh_path = None
+    # ssh_path = 'lumi:/scratch/project_462000451/gene_out/ped2_safescan/scanfiles0012/scan.log'
+    remote_path = "/scratch/project_462000451/gene_out/gene_auto/testing_batchscans"
+    host = 'lumi'
+    #data_set = ScanData('100_3p', parser, ssh_path=ssh_path)
+    data_set = ScanData('testing_batchscans2', parser, host, remote_path=remote_path)
+    
 
     print('HEAD',data_set.head)
     print('POINTS', data_set.x)
-    print('GROWTHRATE', data_set.growthrate)
-    print('FREQUENCY', data_set.frequency)    
+    print('GROWTHRATE', data_set.growthrates)
+    print('FREQUENCY', data_set.frequencies)    
 
-    print(df)
+    print('df FIRST 5\n',data_set.df.head(5))
 
     
