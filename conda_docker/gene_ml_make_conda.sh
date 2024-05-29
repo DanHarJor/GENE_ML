161afc0cc48bcbc1dcac55cc7e3bef024882d989
danharjor <daniel.harley.jordan@outlook.com>
2024-05-29 11:32:56 +0300

    Removed the save to yml from gene_ml_make_conda.sh

--- conda_docker/gene_ml_make_conda.sh
+++ conda_docker/gene_ml_make_conda.sh
@@ -4,4 +4,3 @@
 conda create -n gene_ml -c conda-forge -c pytorch -c anaconda optuna ipykernel matplotlib pandas numpy scikit-learn tqdm nb_conda_kernels f90nml xgboost -y
 conda activate gene_ml
 pip install GPy==1.13.1
-conda env export --name gene_ml > gene_ml_env.yml
