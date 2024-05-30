e119b5e3fd7c87bd5e4096bde74cb4c6ed645970
MarcinMinkowski <minkowski.m@gmail.com>
2024-05-30 13:20:58 +0300

    Neural network

--- gene_ml/models/NN.py
+++ gene_ml/models/NN.py
@@ -1,4 +1,16 @@
 import torch
+from torch import nn
+from .base import Model
 
-class NN(Model):
+class NN(Model, nn.Module):
+    def __init__(self):
+        super(NN, self).__init__()
+        self.linear1 = nn.Linear(8,30)
+        self.relu = nn.ReLU()
+        self.linear2 = nn.Linear(30,2)
     
+    def forward(self,x):
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.linear2(x)
+        return x
