c89b607adddda7e4ad384a1a813040e0455e39b5
danharjor <daniel.harley.jordan@outlook.com>
2024-05-30 14:00:15 +0300

    added in xgboost

--- gene_ml/models/XGBoost.py
+++ gene_ml/models/XGBoost.py
@@ -1,31 +1,22 @@
+from xgboost import XGBRegressor 
 from .base import Model
-from GPy.models import GPRegression
-from GPy.kern import RBF
+
 import numpy as np
 
-class GPR(Model):
+class XGBoost(Model):
     def __init__(self):
-        self.regressor = None
+        self.regressor = XGBRegressor(objective='reg:squarederror')
 
     def train(self, x, y):
-        print('GPR is a parameterless approach and does not have a training step. Instead the hyperparameters are tuned to the data.')
-    
+        self.regressor.fit(x,y)
     def tune_hypers(self, x, y):
-        x = np.array(x)
-        y = np.array(y)[:, None] # GPy needs a 2D array
-        print('OPTIMISING THE HYPERPERS')
-        kernel = RBF(input_dim=len(x[0]), variance=1., lengthscale=10.,ARD=True)
-        self.regressor = GPRegression(x, y, kernel, noise_var=1.0)
-        print('CURRENT HYPERS:\n', self.regressor)
-        print('OPTIMISING THE HYPERPERS:')
-        self.regressor.optimize_restarts(num_restarts = 3)
-        print('RESULTING HYPERS:\n',self.regressor)
-    
+        raise NotImplemented
     def predict(self, x):
-        y_predict, y_error = self.regressor.predict(np.array(x))
-        # print(y_predict.shape)
-        # print(y_predict) 
-        return y_predict[:,0], y_error[:,0]
+        y_predict = self.regressor.predict(np.array(x))
+        return y_predict
+    
+    def sensitivities(self):
+        return self.regressor.get_fscore()
     
 if __name__ == '__main__':
     print('hello world')
