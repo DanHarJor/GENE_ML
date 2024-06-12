54465c079b7f921bc58dd084e623dc3624950bcc
MarcinMinkowski <minkowski.m@gmail.com>
2024-06-12 15:07:59 +0300

    Train method in NN

--- gene_ml/models/NN.py
+++ gene_ml/models/NN.py
@@ -2,15 +2,40 @@ import torch
 from torch import nn
 from .base import Model
 
-class NN(Model, nn.Module):
+class NN(nn.Module, Model):
     def __init__(self):
         super(NN, self).__init__()
         self.linear1 = nn.Linear(8,30)
         self.relu = nn.ReLU()
-        self.linear2 = nn.Linear(30,2)
+        self.linear2 = nn.Linear(30,1)
     
     def forward(self,x):
         x = self.linear1(x)
         x = self.relu(x)
         x = self.linear2(x)
         return x
+    
+    def train(self,dataloader,train_batch_size):
+        loss_fn = nn.MSELoss()
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
+
+        size = len(dataloader.dataset)
+        num_batches = len(dataloader)
+        
+        total_loss = 0.0
+
+        for batch, (X, y) in enumerate(dataloader):
+            pred = self(X)
+            loss = loss_fn(pred,y)
+
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            loss, current = loss.item(), batch * train_batch_size + len(X)
+            total_loss = total_loss + loss
+            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+        total_loss = total_loss/num_batches
+        return total_loss
+
+        
