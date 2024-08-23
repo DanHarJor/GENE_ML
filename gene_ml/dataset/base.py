9507d244b84717eb52ee87887a688ffd6f50dccc
danharjor <daniel.harley.jordan@outlook.com>
2024-08-23 17:31:39 +0300

    After did the ssg_test.ipynb to make sure my implimentation worked on a simple problem. It did work perferctly on a simple problem

--- gene_ml/dataset/base.py
+++ gene_ml/dataset/base.py
@@ -2,7 +2,10 @@ import pickle
 
 class DataSet():
     def __init__(self):
-        None
+        self.x = None
+        self.growthrates = None
+        self.frequencies = None
+        self.run_time = None
     
     def save(self, path):
         pickle.dump(self, open(path, 'wb'))
