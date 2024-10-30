35bb3eedcb930f840d6c02f88540c5036d097683
danharjor <daniel.harley.jordan@outlook.com>
2024-10-30 21:53:55 +0200

    after reconciting UQ and deeplasma

--- conda_docker/gene_ml_load_conda.sh
+++ conda_docker/gene_ml_load_conda.sh
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 conda env create --name gene_ml --file=gene_ml_env_stable.yml
+=======
+conda env create --name gene_ml --file=DEEPlasma/GENE_ML/conda_docker/gene_ml_env_stable.yml
+>>>>>>> 3e0e323 (I made sure the docker works well by loading a conda env rather than making it every time)
