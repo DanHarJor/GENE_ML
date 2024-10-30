e9b64fece41e0e0f74c6eaeeb1646c3f6c190391
danharjor <daniel.harley.jordan@outlook.com>
2024-10-30 22:00:00 +0200

    update, reselutions

--- conda_docker/gene_ml_load_conda.sh
+++ conda_docker/gene_ml_load_conda.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 conda env create --name gene_ml --file=gene_ml_env_stable.yml
-=======
-conda env create --name gene_ml --file=DEEPlasma/GENE_ML/conda_docker/gene_ml_env_stable.yml
->>>>>>> 3e0e323 (I made sure the docker works well by loading a conda env rather than making it every time)
