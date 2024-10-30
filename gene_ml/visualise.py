2f271a96365e88cbc3f5f0706a9c6efd754a4e3e
danharjor <daniel.harley.jordan@outlook.com>
2024-10-30 22:00:00 +0200

    update, reselutions

--- gene_ml/visualise.py
+++ gene_ml/visualise.py
@@ -2,6 +2,28 @@ from decimal import Decimal
 from matplotlib import pyplot as plt
 import numpy as np
 
+# def residual_plot(ax, fig, y_true, y_predicted, var_name, title=None, y_pred_err=None):
+#     mse=np.mean((y_true-y_predicted)**2)
+#     cb = ax.scatter(y_true, y_predicted, marker='.', c=y_pred_err, cmap='summer')
+#     if type(y_pred_err)!=type(None):
+#         ax.errorbar(y_true, y_predicted, yerr=y_pred_err, fmt='.', ecolor='red')
+#         cb = ax.scatter(y_true, y_predicted, marker='.', c=y_pred_err, cmap='summer', zorder=10)
+#         fig.colorbar(cb, ax=ax)
+        
+#     else:
+#         ax.scatter(y_true, y_predicted, marker='.')
+     
+
+#     print('MAX',np.max(y_true),np.max(y_predicted))
+#     ref = np.linspace(np.min((np.min(y_true),np.min(y_predicted))),np.max((np.max(y_true),np.max(y_predicted))), 100).astype(float)
+#     print(np.max(ref))
+#     ax.plot(ref,ref, 'r')
+#     ax.set_xlabel(f'True {var_name}')
+#     ax.set_ylabel(f'Predicted {var_name}')
+#     if title != None:
+#         ax.set_title(title)
+#     ax.annotate(f'MSE: {Decimal(mse):.2E}',
+#             xy=(.6, .02), xycoords='axes fraction',fontsize=10)
 
 def residual_plot(ax, fig, y_true, y_predicted, var_name, title=None, y_pred_err=None, font_size=None):
     residuals = y_true - y_predicted
